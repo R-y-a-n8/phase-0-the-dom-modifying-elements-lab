e4659a5baa8351bfc86517d378458176c974cc83
devRyan <ryan.kinuthia@student.moringaschool.com>
2023-10-16 16:02:21 +0300

    modify

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+// Get a reference to the 'body' element in the DOM
+const body = document.body;
+
+// Create a new paragraph element
+const paragraph = document.createElement('p');
+
+// Set the text content of the paragraph
+paragraph.textContent = 'Hello, World!';
+
+// Append the paragraph to the 'body' element
+body.appendChild(paragraph);
